ba7c6a9f4bf23760327fba3c0bbd7920d42f4e47
gw2083 <gw2083@naver.com>
2022-10-13 18:18:40 +0900

    숫자로 변환 구글링(숙제)

--- JS001/js/main09.js
+++ JS001/js/main09.js
@@ -7,5 +7,5 @@ const btnCacl = document.querySelector("button");
 btnCacl?.addEventListener("click", () => {
   const num1 = inputNum1.value;
   const num2 = inputNum2.value;
-  spanSum.innerText = num1 + num2;
+  spanSum.innerText = Number(num1) + Number(num2);
 });
