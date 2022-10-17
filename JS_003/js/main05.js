125b0e715a274c105ea1a57c2a77b19a5c528079
gw2083 <gw2083@naver.com>
2022-10-17 12:51:59 +0900

    class

---
+++ JS_003/js/main05.js
@@ -0,0 +1,13 @@
+let num1 = 30;
+document.writeln(num1);
+
+// 변수에 값 할당(저장, 대입)
+// 변수에 값을 저장하면 이전에 어떤 값이 저장되어 있던지 상관없이 모두 clear 되고 새로 저장된 값만 저장
+num1 = 40;
+document.writeln(num1);
+
+num1 = num1 + 10;
+num1 = num1 + 10;
+num1 = num1 + 10;
+num1 = num1 + 10;
+num1 = num1 + 10;
