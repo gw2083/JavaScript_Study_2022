3a45d0bd8ea1d467276d169b7c104e5ff2a214e3
gw2083 <gw2083@naver.com>
2022-10-21 15:25:58 +0900

    로또번호 매스랜덤

---
+++ JS_007/js/main02.js
@@ -0,0 +1,89 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const btnAdd = document.querySelector("button.add");
+  const inputs = document.querySelectorAll("input");
+  const tbodyAddr = document.querySelector("tbody.addr");
+  let addrList = [];
+
+  const strAddr = localStorage.getItem("myaddr");
+  addrList = JSON.parse(strAddr);
+
+  if (!addrList) {
+    addrList = [];
+    return false;
+  }
+
+  for (i = 0; i < addrList.length; i++) {
+    const tr = document.createElement("TR");
+    let td = document.createElement("TD");
+    td.textContent = addrList[i].number;
+    tr.appendChild(td);
+
+    td = document.createElement("TD");
+    td.textContent = addrList[i].name;
+    tr.appendChild(td);
+
+    td = document.createElement("TD");
+    td.textContent = addrList[i].kor;
+    tr.appendChild(td);
+
+    td = document.createElement("TD");
+    td.textContent = addrList[i].eng;
+    tr.appendChild(td);
+
+    td = document.createElement("TD");
+    td.textContent = addrList[i].math;
+    tr.appendChild(td);
+
+    td = document.createElement("TD");
+    td.textContent = addrList[i].total;
+    tr.appendChild(td);
+
+    td = document.createElement("TD");
+    td.textContent = addrList[i].avg;
+    tr.appendChild(td);
+
+    tbodyAddr.appendChild(tr);
+  }
+
+  const addrInput = () => {
+    let total =
+      Number(inputs[2].value) +
+      Number(inputs[3].value) +
+      Number(inputs[4].value);
+    let avg = Math.round(total / 3);
+
+    const addr = {
+      number: inputs[0].value,
+      name: inputs[1].value,
+      kor: inputs[2].value,
+      eng: inputs[3].value,
+      math: inputs[4].value,
+      total: total,
+      avg: avg,
+    };
+
+    addrList.push(addr);
+
+    localStorage.setItem("myaddr", JSON.stringify(addrList));
+
+    const tr = document.createElement("TR");
+
+    for (let i = 0; i < inputs.length; i++) {
+      const td = document.createElement("TD");
+      td.textContent = inputs[i].value;
+      tr.appendChild(td);
+    }
+
+    let td = document.createElement("TD");
+    td.textContent = total;
+    tr.appendChild(td);
+
+    td = document.createElement("TD");
+    td.textContent = avg;
+    tr.appendChild(td);
+
+    tbodyAddr.appendChild(tr);
+  };
+
+  btnAdd?.addEventListener("click", addrInput);
+});
