85c8d7d849cdae383860dfb6196594e52592371e
gw2083 <gw2083@naver.com>
2022-10-25 21:58:47 +0900

    과제끄트트으응ㅅ

--- JS_009/js/main23.js
+++ JS_009/js/main23.js
@@ -4,6 +4,7 @@ const mainSection = document.querySelector("section.main");
 const evenSection = document.querySelector("section.even");
 const oddSection = document.querySelector("section.odd");
 const btn = document.querySelector("button.btn");
+const evenDiv = document.querySelector("section.even div");
 
 const makeNums = () => {
   for (let i = 0; i < 10; i++) {
@@ -22,17 +23,21 @@ const viewNums = () => {
     mainSection.appendChild(divs);
   });
 
+  const even = document.createElement("DIV")
+  even.textContent="짝수"
+  evenSection.appendChild(even)
   nums.forEach((num) => {
     if (num % 2 === 0) {
-      const even = document.createElement("DIV");
-      even.textContent = 짝수;
       let divs = document.createElement("DIV");
       divs.textContent = num;
-
       evenSection.appendChild(divs);
     }
   });
 
+
+  const odd = document.createElement("DIV")
+  odd.textContent="홀수"
+  oddSection.appendChild(odd)
   nums.forEach((num) => {
     if (num % 2 != 0) {
       let divs = document.createElement("DIV");
